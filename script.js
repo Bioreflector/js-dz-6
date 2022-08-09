b3b121c5c55dc9aa0b32aa799fa54a84fd64a920
Alexandr <bioreflector@gmail.com>
2022-08-09 14:01:15 +0300

    Task 5 compleated -Define a Constructor Function-

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Task 4 make Code More Reusable with the this Keyword
-let dog = {
-    name: "Spot",
-    numLegs: 4,
-  };
-  dog.sayLegs = function(){
-      return `This dog has ${this.numLegs} legs.`
-    }
-   dog.sayLegs()
+// Task 5 define a Constructor Function
+function Dog(){
+    this.name = 'Jack'
+    this.color = 'black'
+    this.numLegs = 4
+  }
