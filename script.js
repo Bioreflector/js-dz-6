dff95c274b1e110653c2789607a8b4a62fb47150
Alexandr <bioreflector@gmail.com>
2022-08-09 13:59:25 +0300

    Task 4 compleated -Make Code More Reusable with the this Keyword-

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Task 3 Create a Method on an Object
+// Task 4 make Code More Reusable with the this Keyword
 let dog = {
     name: "Spot",
     numLegs: 4,
   };
   dog.sayLegs = function(){
-    return `This dog has ${dog.numLegs} legs.`
-  }
-  dog.sayLegs()
+      return `This dog has ${this.numLegs} legs.`
+    }
+   dog.sayLegs()
