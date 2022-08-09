be4e5efa0cb02694073d1bacace55b46f98bc81f
Alexandr <bioreflector@gmail.com>
2022-08-09 14:09:50 +0300

    Task 11 compleated -Iterate Over All Properties-

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// Task 10 use Prototype Properties to Reduce Duplicate Code
+// Task 11 iterate Over All Properties
 function Dog(name) {
     this.name = name;
   }
-  Dog.prototype.numLegs = 4
-  let beagle = new Dog("Snoopy");
+  Dog.prototype.numLegs = 4;
+  const beagle = new Dog("Snoopy")
+  
+  const ownProps = [];
+  const prototypeProps = [];
+  for(let property in beagle){
+    if(beagle.hasOwnProperty(property)){
+      ownProps.push(property)
+    }
+    else{
+      prototypeProps.push(property)
+    }
+  }
