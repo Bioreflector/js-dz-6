d5b9e68f7c96c03d35dfe2130ed23677724ee67d
Alexandr <bioreflector@gmail.com>
2022-08-09 16:56:59 +0300

    Task 23 compleated -Use a Mixin to Add Common Behavior Between Unrelated Objects-

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-// Task 22 Override Inherited Methods
-function Bird() { }
-Bird.prototype.fly = function() { return "I am flying!"; };
-function Penguin() { }
-Penguin.prototype = Object.create(Bird.prototype);
-Penguin.prototype.constructor = Penguin;
-Penguin.prototype.fly = function() { return "Alas, this is a flightless bird"; };
-let penguin = new Penguin();
-console.log(penguin.fly());
+// Task 23 use a Mixin to Add Common Behavior Between Unrelated Objects
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  let glideMixin= function(obj){
+    obj.glide = function(){
+      console.log('Gliding, wooosh!')
+    }
+  }
+  glideMixin(bird)
+  glideMixin(boat)
+  bird.glide()
+  boat.glide()
 
 
 
