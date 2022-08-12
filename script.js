0e75d8e0c965669affee6686d2bd4bc94e195317
Bioreflector <Bioreflector@gmail.com>
2022-08-12 14:44:35 +0300

    Task 24 compleadet -Use Closure to Protect Properties Within an Object from Being Modified Externally-

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-// Task 23 use a Mixin to Add Common Behavior Between Unrelated Objects
-let bird = {
-    name: "Donald",
-    numLegs: 2
-  };
-  
-  let boat = {
-    name: "Warrior",
-    type: "race-boat"
-  };
-  let glideMixin= function(obj){
-    obj.glide = function(){
-      console.log('Gliding, wooosh!')
+// Task 24 use a Mixin to Add Common Behavior Between Unrelated Objects
+function Bird() {
+    let weight = 15;
+     this.getWeight = function() {
+      return weight;
     }
   }
-  glideMixin(bird)
-  glideMixin(boat)
-  bird.glide()
-  boat.glide()
 
 
 
