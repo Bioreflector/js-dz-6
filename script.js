c0d8a1e9b14c887b6f51cfd0d0ff6dfa03402a5e
Bioreflector <Bioreflector@gmail.com>
2022-08-12 14:52:03 +0300

    Task 25 compleated -Understand the Immediately Invoked Function Expression (IIFE)-

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// Task 24 use a Mixin to Add Common Behavior Between Unrelated Objects
-function Bird() {
-    let weight = 15;
-     this.getWeight = function() {
-      return weight;
-    }
-  }
+// Task 25 use Understand the Immediately Invoked Function Expression (IIFE)
+(function (){
+    console.log("A cozy nest is ready")
+})()
 
 
 
